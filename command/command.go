eace1dbcd5baf5941e33f72856a2862c4002501b
ninesl <lance.nines@gmail.com>
2025-01-05 10:40:05 -0500

    basic framework for arg processing + help msg

---
+++ command/command.go
@@ -0,0 +1,48 @@
+package command
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/ninesl/mindtick/messages"
+	"github.com/ninesl/mindtick/store"
+)
+
+var useHelpMsg = fmt.Sprintf("use %s for more information\n", messages.ColorizeStr("mindtick help", messages.BrightGreen))
+
+func helpLine(command, description string) string {
+	return fmt.Sprintf(
+		"%s%s\t%s\n",
+		messages.ColorizeStr("  ", messages.BrightGreen),
+		messages.ColorizeStr(command, messages.BrightGreen),
+		description,
+	)
+}
+
+func help() {
+	var sb strings.Builder
+	sb.WriteString("Usage: ")
+	sb.WriteString(messages.ColorizeStr("mindtick <command>\n", messages.BrightGreen))
+	sb.WriteString("Commands:\n")
+	sb.WriteString(helpLine("new", "Create a new mindtick file in the current directory"))
+	sb.WriteString(helpLine("help", "Display this help message"))
+
+	fmt.Print(sb.String())
+}
+
+func ProcessArgs() {
+	if len(os.Args) < 2 {
+		fmt.Printf("No arguments provided, %s", useHelpMsg)
+		return
+	}
+
+	switch os.Args[1] {
+	case "new":
+		store.New()
+	case "help":
+		help()
+	default:
+		fmt.Printf("Unknown command %s, %s", os.Args[1], useHelpMsg)
+	}
+}
