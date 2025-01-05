08d769c0334eb11b734c0996a527f2518041d3af
ninesl <lance.nines@gmail.com>
2025-01-05 13:09:07 -0500

    process message

--- command/command.go
+++ command/command.go
@@ -9,7 +9,10 @@ import (
 	"github.com/ninesl/mindtick/store"
 )
 
-var useHelpMsg = fmt.Sprintf("use %s for more information\n", messages.ColorizeStr("mindtick help", messages.BrightGreen))
+var (
+	useHelpMsg    = fmt.Sprintf("use %s for more information\n", messages.ColorizeStr("mindtick help", messages.BrightGreen))
+	messagePrefix = '>'
+)
 
 func helpLine(command, description string) string {
 	return fmt.Sprintf(
@@ -55,7 +58,43 @@ func ProcessArgs() {
 		}
 	case "help":
 		help()
+
+	// list of note commands is hard to maintain and extend with current implementation
+	case "win": // new note, usage is like `mindtick {msgType} "note content"`
+	case "task":
+	case "note":
+	case "fix":
+		processMessage()
 	default:
 		fmt.Printf("unknown mindtick argument %s, %s", messages.ColorizeStr(os.Args[1], messages.BrightPurple), useHelpMsg)
+	}
+}
+
+func processMessage() {
+	if len(os.Args) < 3 {
+		fmt.Printf("mindtick %s must have a message, %s", messages.ColorizeStr(os.Args[1], messages.BrightPurple), useHelpMsg)
+		return
+	}
+	if rune(os.Args[2][0]) != messagePrefix {
+		tip := fmt.Sprintf("mindtick %s >{your message here}", os.Args[1])
+		fmt.Printf("mindtick messages must start with >. example usage: %s\n%s", messages.ColorizeStr(tip, messages.BrightGreen), useHelpMsg)
+		return
+	}
+	// concat all arguments after the msgType
+	var argMsgs []string
+	for i := 3; i < len(os.Args); i++ {
+		argMsgs = append(argMsgs, os.Args[i])
+	}
+	var argMsg = strings.Join(argMsgs, " ")
+	argMsg = strings.Replace(argMsg, ">", "", 1) // remove the > from the beginning
+	msg, err := messages.NewMessage(os.Args[1], argMsg)
+	if err != nil {
+		fmt.Printf("%s, %s", messages.ColorizeStr(err.Error(), messages.BrightRed), useHelpMsg)
+		return
+	}
+	err = store.AddMessage(msg)
+	if err != nil {
+		fmt.Printf("%s, %s", messages.ColorizeStr(err.Error(), messages.BrightRed), useHelpMsg)
+		return
 	}
 }
